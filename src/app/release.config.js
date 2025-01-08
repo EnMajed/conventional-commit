178d22d5d3285ce22ce3b4b8ab7b87c627dcbab2
EnMajed <92181177+EnMajed@users.noreply.github.com>
2025-01-08 18:38:37 +0300

    refactor: refactor the code

---
+++ src/app/release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    branches: ['main'], // فرع العمل الأساسي
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      '@semantic-release/github',
+      '@semantic-release/git',
+    ],
+  };
