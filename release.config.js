20973fcfdfc9dec7258b668a56930d300293c697
EnMajed <92181177+EnMajed@users.noreply.github.com>
2025-01-09 10:12:38 +0300

    feat : add release config

--- release.config.js
+++ release.config.js
@@ -1,10 +1,12 @@
 module.exports = {
     branches: ['main'], // فرع العمل الأساسي
+    repositoryUrl: 'https://github.com/EnMajed/conventional-commit.git',
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
       '@semantic-release/changelog',
       '@semantic-release/github',
       '@semantic-release/git',
+      '@semantic-release/npm',
     ],
   };
